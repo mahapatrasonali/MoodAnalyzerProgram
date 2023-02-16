8a1b36bb4afddc36874042fce50cfc517b32e4a5
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-16 21:06:06 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Mood Analyzer Program");
+        }
+    }
+}
