bdd6540bda8611f02f9895572d278f502c8d2ab9
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-18 12:35:49 +0530

    UC1 Added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MoodAnalyzerProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("How is your mood Happy or Sad");
+            string message = Console.ReadLine();
+            MoodAnalyser moodAnalyzer = new MoodAnalyser();
+            Console.WriteLine(moodAnalyzer.AnalyseMood(message));
+        }
+    }
+}
